cb495307f076c28ecf112e7108e2c4f80e995a7c
feibi-allen <146375798+feibi-allen@users.noreply.github.com>
2025-02-20 15:34:33 +0000

    route for account page added, links in nav bar to homepage and account page added

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,7 @@ from flask import render_template
 @app.route('/')
 def mainPage():
     return render_template('mainPage.html')
+
+@app.route('/account')
+def account():
+    return render_template('account.html')
