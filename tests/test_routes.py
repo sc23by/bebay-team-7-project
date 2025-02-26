f98f9456b5fd312011abac5650f51a89558f0986
feibi-allen <146375798+feibi-allen@users.noreply.github.com>
2025-02-26 13:20:35 +0000

    client set up for db entries

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -3,7 +3,8 @@ import os
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from app import app
+from app import app, db, bcrypt
+from app.models import User
 import pytest
 from colours import Colours
 
@@ -15,8 +16,19 @@ loggedInClient: client who has logged in
 @pytest.fixture
 def client():
     print("\nSetting up the test client")
+    
+    # testing mode enabled, use memory db not the real one, dissable security
+    app.config['TESTING'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    app.config['WTF_CSRF_ENABLED'] = False
     with app.test_client() as client:
+        with app.app_context():
+            # create temp db
+            db.create_all()
         yield client
+        with app.app_context():
+            # post test cleanup
+            db.drop_all()
     print("\nTearing down the test client")
 
 @pytest.fixture
@@ -24,11 +36,19 @@ def loggedInClient(client):
     client.get('/login')
     return client
 
+
+'''
+Testing routes with logged out client
+'''
 def test_home(client):
     print(f"{Colours.YELLOW}Testing homepage - logged out:{Colours.RESET}")
     response = client.get('/')
     assert response.status_code == 200
 
+
+'''
+Testing routes with logged out client
+'''
 def test_home_authenticated(loggedInClient):
     print(f"{Colours.YELLOW}Testing homepage - logged in:{Colours.RESET}")
     response = loggedInClient.get('/')
