75b7e7e3431bbbce9d291d792736f178e93a4d9e
feibi-allen <146375798+feibi-allen@users.noreply.github.com>
2025-03-19 14:26:23 +0000

    expert can see assigned items

--- tests/test_authenticate_items.py
+++ tests/test_authenticate_items.py
@@ -3,6 +3,7 @@
 from app import app, db,bcrypt
 from app.models import WaitingList, Item, User, ExpertAvailabilities
 from datetime import datetime, timedelta
+from flask_login import current_user
 import io
 from werkzeug.datastructures import FileStorage
 
@@ -151,3 +152,39 @@ def test_manager_assign_expert(loggedInClientP3):
         expert = User.query.get(10)
         item = Item.query.filter_by(item_id=1).first()
         assert item.expert == expert
+
+
+def test_expert_vue_assignment(loggedInClientP2):
+    print(f"{Colours.YELLOW}Testing authenticate items - expert can see item assigned to them:{Colours.RESET}")
+
+    with app.app_context():
+        item = Item(
+            item_id = 1,
+            seller_id=1,
+            item_name="apel",
+            description="Wow what an item",
+            minimum_price=10.99,
+            shipping_cost=5.50,
+            days=3,
+            hours=2,
+            minutes=30,
+            item_image="test_image.jpg",
+            date_time=None, # not authenticated yet
+            expiration_time=None, # not authenticated yet
+            approved=False,
+            expert_payment_percentage=10.0,
+            site_fee_percentage=1.0,
+            expert_fee_percentage=4.0,
+            sold=False,
+            expert = current_user)
+        
+        db.session.add(item)
+        db.session.commit()
+
+        waiting_list_entry = WaitingList(item_id=item.item_id)
+        db.session.add(waiting_list_entry)
+        db.session.commit()
+            
+    response = loggedInClientP2.get("/expert/assignments")
+    assert response.status_code == 200
+    assert b"apel" in response.data
