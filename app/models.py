aa1e2f4a2bbee2f8502dd165958293ef4bd249bc
LeynaTiji <146225194+LeynaTiji@users.noreply.github.com>
2025-03-20 17:39:55 +0000

    Added css and bootstrap to style item details page

--- app/models.py
+++ app/models.py
@@ -24,7 +24,7 @@ class User(UserMixin, db.Model):
     profile_picture = db.Column(db.String(255), nullable=False, default="default_profile.jpg")
 
     watchlist = db.relationship('Item', secondary=Watched_item, backref='watched_by') # allows user to watch multiple items
-    items = db.relationship('Item',foreign_keys='Item.seller_id',backref='seller',lazy=True)
+    items = db.relationship('Item',foreign_keys='Item.seller_id',backref='seller', lazy=True)
 
 # Expert model
 class ExpertAvailabilities(db.Model):
